3e692bfc2e1bf463d30c2c969d36c001a91d2c4d
Bruno De Masi <brunodemasi1@gmail.com>
2023-03-31 19:32:17 -0300

    tailwind, prettier and eslint

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# nextjs-boilerplate
+## NextJS boilerplate
+
+Using for quickstarting a Next.js project, it has already been setup with the following:
+
+- prettier
+- eslint
+- tailwindcss
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '#000',
+      secondary: '#fff',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
